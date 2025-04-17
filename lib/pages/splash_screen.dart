5f0f0633fbebee4fc2ee91e3f2999eaea1e5842e
Lee Changjae <cjlee@cyglobal.net>
2025-04-17 16:52:49 +0900

    ⚡️ Feat: add auto login

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'package:bookstar_app/main.dart';
 import 'package:bookstar_app/pages/auth/screen/login_page.dart';
+import 'package:bookstar_app/pages/home/home_page.dart';
+import 'package:bookstar_app/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -17,11 +21,40 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     if (!mounted) return;
     //TODO:여기서 자동로그인 설정 및 서버에서 토큰받아오기
-    Future.delayed(
+
+    autoLogin();
+
+    // Future.delayed(
+    //   Duration(seconds: 2),
+    //   () {
+    //     if (!mounted) return;
+    //     context.go(LoginPage.routePath);
+    //   },
+    // );
+  }
+
+  Future<void> autoLogin() async {
+    String? accessToken = prefs.getString('accessToken');
+    int? memberId = prefs.getInt('memberId');
+    String? profileImage = prefs.getString('profileImage');
+    String? nickName = prefs.getString('nickName');
+    await Future.delayed(
       Duration(seconds: 2),
-      () {
-        if (!mounted) return;
-        context.go(LoginPage.routePath);
+      () async {
+        if (accessToken != null) {
+          if (!mounted) return;
+          context.read<UserProvider>().setUserInfo(
+                userId: memberId,
+                nickName: nickName,
+                profileImage: profileImage,
+                accessToken: accessToken,
+              );
+          context.go(HomePage.routePath);
+        } else {
+          await prefs.clear();
+          if (!mounted) return;
+          context.go(LoginPage.routePath);
+        }
       },
     );
   }

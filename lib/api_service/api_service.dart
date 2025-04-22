f9a48fcdbcc048d780aaea19191a403c71a7b5a6
Lee Changjae <cjlee@cyglobal.net>
2025-04-22 15:44:48 +0900

    ⚡️ Feat: add new server api sevice

---
+++ lib/api_service/api_service.dart
@@ -0,0 +1,57 @@
+import 'package:bookstar_app/api_service/dio_client.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class ApiService {
+  static Future<Response> apiPostService({
+    required String path,
+    Map<String, dynamic>? body,
+  }) async {
+    late Response response;
+    try {
+      response = await dio.post(path, data: body);
+    } catch (e) {
+      debugPrint('API에러 - $e');
+    }
+    return response;
+  }
+
+  static Future<Response> apiGetService({
+    required String path,
+    Map<String, dynamic>? body,
+  }) async {
+    late Response response;
+    try {
+      response = await dio.get(path, data: body);
+    } catch (e) {
+      debugPrint('API에러 - $e');
+    }
+    return response;
+  }
+
+  static Future<Response> apiPutService({
+    required String path,
+    Map<String, dynamic>? body,
+  }) async {
+    late Response response;
+    try {
+      response = await dio.put(path, data: body);
+    } catch (e) {
+      debugPrint('API에러 - $e');
+    }
+    return response;
+  }
+
+  static Future<Response> apiDeleteService({
+    required String path,
+    Map<String, dynamic>? body,
+  }) async {
+    late Response response;
+    try {
+      response = await dio.delete(path, data: body);
+    } catch (e) {
+      debugPrint('API에러 - $e');
+    }
+    return response;
+  }
+}
--- lib/global/login_cubit/login_cubit.dart
+++ lib/global/login_cubit/login_cubit.dart
@@ -1,6 +1,10 @@
-import 'package:bookstar_app/api_service/dio_client.dart';
+import 'package:bookstar_app/api_service/api_service.dart';
 import 'package:bookstar_app/main.dart';
+import 'package:bookstar_app/model/common_dto.dart';
+import 'package:bookstar_app/model/login_dto.dart';
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'login_state.dart';
@@ -13,22 +17,30 @@ class LoginCubit extends Cubit<LoginState> {
     required String? idToken,
     required String provider,
   }) async {
-    final response = await dio.post(
-      'auth/login',
-      data: {
+    try {
+      final Response response =
+          await ApiService.apiPostService(path: 'auth/login', body: {
         'idToken': idToken,
         'providerType': provider,
-      },
-    );
-    final String accessToken = response.data['data']['accessToken'];
-    final String profileImage = response.data['data']['profileImage'] ?? '';
-    final String refreshToken = response.data['data']['refreshToken'];
-    final String nickName = response.data['data']['nickName'];
-    final int memberId = response.data['data']['memberId'];
-    await prefs.setString('accessToken', accessToken);
-    await prefs.setString('profileImage', profileImage);
-    await prefs.setInt('memberId', memberId);
-    await prefs.setString('nickName', nickName);
-    await prefs.setString('refreshToken', refreshToken);
+      });
+
+      if (response.data is Map<String, dynamic>) {
+        final CommonDto<LoginDto> commonResponse = CommonDto<LoginDto>.fromJson(
+          response.data,
+          (jsonData) => LoginDto.fromJson(jsonData as Map<String, dynamic>),
+        );
+        final LoginDto loginData = commonResponse.data;
+
+        await prefs.setString('accessToken', loginData.accessToken);
+        await prefs.setString('profileImage', loginData.profileImage);
+        await prefs.setInt('memberId', loginData.memberId);
+        await prefs.setString('nickName', loginData.nickName);
+        await prefs.setString('refreshToken', loginData.refreshToken);
+      } else {
+        debugPrint('앱 로그인 실패 - json형식 오류');
+      }
+    } catch (e) {
+      debugPrint('앱 로그인 실패 - $e');
+    }
   }
 }

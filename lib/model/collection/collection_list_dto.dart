a5b2dcd44bb488dd8b47e2415ddd727c8676dbe0
Carl-Lee91 <ckdwkdl91@gmail.com>
2025-05-20 17:03:28 +0900

    ✨ Feat: add collection dto

---
+++ lib/model/collection/collection_list_dto.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+class CollectionListDto extends Equatable {
+  final int collectionId;
+  final String name;
+  final String description;
+  final List<String> images;
+
+  const CollectionListDto({
+    required this.collectionId,
+    required this.name,
+    required this.description,
+    required this.images,
+  });
+
+  factory CollectionListDto.fromJson(Map<String, dynamic> json) {
+    return CollectionListDto(
+      collectionId: json['collectionId'] ?? 0,
+      name: json['name'] ?? '',
+      description: json['description'] ?? '',
+      images: json['images'] ?? [],
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        collectionId,
+        name,
+        description,
+        images,
+      ];
+}

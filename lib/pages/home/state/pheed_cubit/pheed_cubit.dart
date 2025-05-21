fa21007be9be4027a7c3b46c73b0e92b52f80a30
Carl-Lee91 <ckdwkdl91@gmail.com>
2025-05-21 16:05:37 +0900

    ✨ Feat: add fetch my pheed cubit

--- lib/pages/home/state/pheed_cubit/pheed_cubit.dart
+++ lib/pages/home/state/pheed_cubit/pheed_cubit.dart
@@ -11,7 +11,7 @@ part 'pheed_state.dart';
 class PheedCubit extends Cubit<PheedState> {
   PheedCubit() : super(PheedState());
 
-  Future<void> fetchFeedItems() async {
+  Future<void> fetchMyFeedItems() async {
     try {
       final Response response = await ApiService.apiGetService(
         path: 'pheed/me',
@@ -24,7 +24,7 @@ class PheedCubit extends Cubit<PheedState> {
       final List<PheedItemDto> items = rawList.map((itemJson) {
         return PheedItemDto.fromJson(itemJson);
       }).toList();
-      emit(state.copyWith(pheedItems: items));
+      emit(state.copyWith(pheedMyItems: items));
       debugPrint("pheed 호출 및 파싱 성공: ${items.length} items loaded.");
     } catch (e) {
       debugPrint('fetchFeedItems 실패 - $e');
@@ -74,30 +74,23 @@ class PheedCubit extends Cubit<PheedState> {
     }
   }
 
-  // Future<void> fetchAiRecommendBook({
-  //   required int userId,
-  // }) async {
-  //   try {
-  //     final response = await http.post(
-  //       Uri.parse('http://15.164.30.67:8000/recommend_books'),
-  //       headers: {'Content-Type': 'application/json'},
-  //       body: jsonEncode({'user_id': userId}),
-  //     );
-
-  //     if (response.statusCode == 200) {
-  //       final decodedData = jsonDecode(utf8.decode(response.bodyBytes));
-  //       final List<dynamic> rawList = decodedData['recommendations'];
-  //       final List<AiRecommedBookDto> items = rawList.map((itemJson) {
-  //         return AiRecommedBookDto.fromJson(itemJson);
-  //       }).toList();
-  //       emit(state.copyWith(aiRecommedBooks: items));
-  //       debugPrint(
-  //           "fetchAiRecommendBook 호출 및 파싱 성공: ${items.length} items loaded.");
-  //     } else {
-  //       debugPrint('fetchAiRecommendBook 실패 - ${response.statusCode}');
-  //     }
-  //   } catch (e) {
-  //     debugPrint('fetchAiRecommendBook 실패 - $e');
-  //   }
-  // }
+  Future<void> fetchFriendsFeedItems() async {
+    try {
+      final Response response = await ApiService.apiGetService(
+        path: 'pheed',
+        options: Options(
+          extra: {'requiresToken': true},
+        ),
+      );
+      final Map<String, dynamic> responseData = response.data;
+      final List<dynamic> rawList = responseData['postItemResponses'];
+      final List<PheedItemDto> items = rawList.map((itemJson) {
+        return PheedItemDto.fromJson(itemJson);
+      }).toList();
+      emit(state.copyWith(pheedItems: items));
+      debugPrint("pheed 호출 및 파싱 성공: ${items.length} items loaded.");
+    } catch (e) {
+      debugPrint('fetchFriendsFeedItems 실패 - $e');
+    }
+  }
 }

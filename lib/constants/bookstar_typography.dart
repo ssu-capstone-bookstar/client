7402f713c78562ca7a935b998a227070f46df122
Carl-Lee91 <ckdwkdl91@gmail.com>
2025-05-26 10:27:44 +0900

    💄 Style: add constant typography

---
+++ lib/constants/bookstar_typography.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/widgets.dart';
+
+class BookstarTypography {
+  // Bookstar Typography Headline
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  static const TextStyle headline2 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  static const TextStyle headline3 = TextStyle(
+    fontSize: 36,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  static const TextStyle headline4 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  static const TextStyle headline5 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+  static const TextStyle headline6 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+
+  // Bookstar Typography Body
+  static const TextStyle body1 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    height: 1.4,
+  );
+  static const TextStyle body2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+  );
+  static const TextStyle body3 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.4,
+  );
+  static const TextStyle body4 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+  );
+  static const TextStyle body5 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  );
+  static const TextStyle body6 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+  static const TextStyle body7 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 1.5,
+  );
+  static const TextStyle body8 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+  static const TextStyle body9 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    height: 1.6,
+  );
+  static const TextStyle body10 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.6,
+  );
+  static const TextStyle body11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    height: 1.7,
+  );
+}

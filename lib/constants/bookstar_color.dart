3d1ab940e5dfbed4942429ce132dedc069382f31
Carl-Lee91 <ckdwkdl91@gmail.com>
2025-05-26 10:27:23 +0900

    💄 Style: add constant color

---
+++ lib/constants/bookstar_color.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BookstarColor {
+  // BookstarColor White Color
+  static const Color whiteColor = Color(0xFFFFFFFF);
+
+  // BookstarColor Black Color
+  static const Color blackColor = Color(0xFF191919);
+
+  // BookstarColor Grey Color
+  static const Color greyColor1 = Color(0xFFFAFAFA);
+  static const Color greyColor2 = Color(0xFFEFF0F2);
+  static const Color greyColor3 = Color(0xFFD4D9DD);
+  static const Color greyColor4 = Color(0xFFAFB1B6);
+  static const Color greyColor5 = Color(0xFF5A5A62);
+  static const Color greyColor6 = Color(0xFF2D2D33);
+
+  // BookstarColor Dim Color
+  static Color dimColor1 = Color(0xFF000000).withValues(alpha: 0.3);
+  static Color dimColor2 = Color(0xFF000000).withValues(alpha: 0.7);
+}
